9eb5b065ce9c0626d99afc96b1c7fd8e61b95e76
Skarleth7 <92055755+Skarleth7@users.noreply.github.com>
2022-05-11 17:42:19 +0200

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -95,6 +95,17 @@ class KMeans:
         elif self.options['km_init'].lower() == 'custom':  # TBImplemented
             pass
 
+        elif self.options['km_init'].lower() == 'backward':
+            row_indexes = np.unique(self.X, return_index=True, axis=0)[1]
+
+            sorted_index = sorted(row_indexes)
+
+            centroids = []
+
+            for indexIT in range(self.K):
+                centroids.append(self.X[sorted_index[indexIT]]
+            self.centroids = np.flipud(np.array(centroids))
+            pass
         self.old_centroids = self.centroids
 
     def get_labels(self):
