7a936a4731ab8fa869c6fa9517104e860390e357
ERlK2021 <92644465+ERlK2021@users.noreply.github.com>
2022-05-13 21:01:13 +0200

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -9,12 +9,12 @@ import operator
 from scipy.spatial.distance import cdist
 
 class KNN:
-    def __init__(self, train_data, labels):
-
-        self._init_train(train_data)
+    def __init__(self, train_data, labels, res="default"):
+                
+        self._init_train(train_data, res)       
         self.labels = np.array(labels)
 
-    def _init_train(self, train_data):
+    def _init_train(self, train_data, res="default"):
         """
         initializes the train data
         :param train_data: PxMxNx3 matrix corresponding to P color images
@@ -23,7 +23,17 @@ class KNN:
 
         #Numpy arrays are float by default. 14400 'cause --> 60x80x3 (RGB)
         #SRC: https://numpy.org/doc/stable/reference/generated/numpy.var.html#:~:text=For%20arrays%20of%20integer%20type,same%20as%20the%20array%20type.
-        self.train_data = np.reshape(np.array(train_data), ((len(train_data)), 14400))
+        
+        if res=="half":
+            train_data = np.resize(train_data,(len(train_data),40,30,3))
+            self.train_data = np.reshape(np.array(train_data), ((len(train_data)), 40*30*3))
+            
+        elif res=="fourth":
+            train_data = np.resize(train_data,(len(train_data),20,15,3))
+            self.train_data = np.reshape(np.array(train_data), ((len(train_data)), 20*15*3))
+            
+        else:
+            self.train_data = np.reshape(np.array(train_data), ((len(train_data)), 60*80*3))
 
     def get_k_neighbours(self, test_data, k):
         """
@@ -78,13 +88,22 @@ class KNN:
         maxNeighbors = np.array(neighbors) #Array into numpy array
         return maxNeighbors
 
-    def predict(self, test_data, k):
+    def predict(self, test_data, k, res="default"):
         """
         predicts the class at which each element in test_data belongs to
         :param test_data: array that has to be shaped to a NxD matrix ( N points in a D dimensional space)
         :param k:         :param k:  the number of neighbors to look at
         :return: the output form get_class (2 Nx1 vector, 1st the classm 2nd the  % of votes it got
         """
-
+        
+        #print("Shape: ",test_data.shape[0],test_data.shape[1],test_data.shape[2],test_data.shape[3] )
+        #test_data= np.resize(test_data,(len(test_data),40,30,3))
+          
+        if res=="half":
+            test_data = np.resize(test_data,(len(test_data),40,30,3))
+                        
+        elif res=="fourth":
+            test_data = np.resize(test_data,(len(test_data),20,15,3))
+                        
         self.get_k_neighbours(test_data, k)
         return self.get_class()
